148faec509593cfa11b699938a5a59f884f1e51b
黄青松 <huangqingsong_5678@hotmail.com>
2017-08-11 14:31:58 +0800

    update

---
+++ Tabs/app/src/main/java/com/hqs/common/view/qstabs/QTabView.java
@@ -0,0 +1,21 @@
+package com.hqs.common.view.qstabs;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by super on 2017/8/11.
+ */
+
+public class QTabView extends LinearLayout {
+
+    public QTabView(Context context) {
+        super(context);
+    }
+
+    public QTabView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
